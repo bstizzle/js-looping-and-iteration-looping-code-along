9293e3b4467adc378d39c673c9f1e76ec89cbb00
bstizzle <whereyouben@gmail.com>
2020-12-09 16:02:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let messages = [];
+    for(let c = 0; c < names.length; c++) {
+        messages.push(`Thank you, ${names[c]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+function countDown(int) {
+    while(int != -1) {
+        console.log(int);
+        int -= 1;
+    }
+}
